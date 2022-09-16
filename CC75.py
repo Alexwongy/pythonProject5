914c2a7f7fde99bfc60df84ecfbdc04125453ee7
Alexwongy <yingtaohuang12@gmail.com>
2022-09-16 16:45:13 +0800

    Just test

---
+++ CC75.py
@@ -0,0 +1,10 @@
+age = input("How old are you ?")
+while True:
+   price = int(age)
+    if price < 3:
+        money = 0
+    if prince >= 3 and <= 12:
+        money = 10
+   else:
+        money = 15
+print(f'It is {money}$ to see this movie.')
