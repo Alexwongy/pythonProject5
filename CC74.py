28f52e898a47f4168364977519d32fe5d716516e
Alexwongy <yingtaohuang12@gmail.com>
2022-09-16 16:13:45 +0800

    Just test

--- CC74.py
+++ CC74.py
@@ -1,4 +1,10 @@
-123
-321
-56489
+prompt = "\nWhat topping would you like on your pizza?"
+prompt += "\nEnter 'quit' when you are finished: "
+while True:
+ topping = input(prompt)
+ if topping != 'quit':
+    print(f'I will add {topping} on your pizza')
+ else:
+     break
+
 
